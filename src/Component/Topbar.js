3a7b66f22240e6c5ac06c9f18dd107930d8c0b84
tung <caothanhtung08082002@gmail.com>
2022-12-28 21:38:33 +0700

    init structure

---
+++ src/Component/Topbar.js
@@ -0,0 +1,5 @@
+function Topbar() {
+  return <p>Topbar</p>;
+}
+
+export default Topbar;
---
+++ src/Page/HomePage.js
@@ -0,0 +1,13 @@
+import Sidebar from "../Component/Sidebar";
+import Topbar from "../Component/Topbar";
+
+function HomePage() {
+  return (
+    <>
+      <Topbar></Topbar>
+      <Sidebar />
+    </>
+  );
+}
+
+export default HomePage;
