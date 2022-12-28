427bf0b610b251e9e1898b86b85e6c5bb5c88027
tung <caothanhtung08082002@gmail.com>
2022-12-28 21:55:39 +0700

    add tailwind

--- src/Component/Topbar.js
+++ src/Component/Topbar.js
@@ -1,5 +1,5 @@
 function Topbar() {
-  return <p>Topbar</p>;
+  return <p className="text-3xl font-bold">Topbar</p>;
 }
 
 export default Topbar;
--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -4,6 +4,7 @@ import Topbar from "../Component/Topbar";
 function HomePage() {
   return (
     <>
+      <h1 className=" text-3xl font-bold underline color-blue">Hello world!</h1>
       <Topbar></Topbar>
       <Sidebar />
     </>
