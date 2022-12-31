7858aee3ae159d9d660398ac5a95a71d0c7a3695
tung <caothanhtung08082002@gmail.com>
2022-12-31 21:02:23 +0700

    add json

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const port = 8000;
-
+// app.use(express.json());
+app.use(morgan("tiny"));
 app.get("/", (req, res) => {
-  res.send("hello");
+  // res.send("hello");
+  res.send({
+    name: "thanh",
+  });
   res.end();
 });
 app.listen(port, function () {
