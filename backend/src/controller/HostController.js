8e752d27ab0d7b10d1742c5417cef4ce0a43af71
tung <caothanhtung08082002@gmail.com>
2023-01-04 11:32:17 +0700

    hoan thanh controller cho host

--- backend/src/controller/HostController.js
+++ backend/src/controller/HostController.js
@@ -1,8 +1,51 @@
+const service = require("../service/Service");
 const HostController = {
-  postRoom(req, res) {},
-  updateRoom(req, res) {},
-  deleteRoom(req, res) {},
-  rentResponse(req, res) {},
+  postRoom(req, res) {
+    let userInfo = req.body.userInfo;
+    let serviceInfo = req.body.serviceInfo;
+    if (service.validateUser(userInfo)) {
+      service.createRoom(serviceInfo);
+      res.send(service.SuccessCode);
+    } else {
+      res.send(service.FailCode);
+    }
+  },
+  updateRoom(req, res) {
+    let userInfo = req.body.userInfo;
+    let serviceInfo = req.body.serviceInfo;
+    if (service.validateUser(userInfo)) {
+      service.updateRoom(serviceInfo);
+      res.send(service.SuccessCode);
+    } else {
+      res.send(service.FailCode);
+    }
+  },
+  deleteRoom(req, res) {
+    let userInfo = req.body.userInfo;
+    let serviceInfo = req.body.serviceInfo;
+    if (service.validateUser(userInfo)) {
+      service.deleteRoom(serviceInfo);
+      res.send(service.SuccessCode);
+    } else {
+      res.send(service.FailCode);
+    }
+  },
+  rentResponse(req, res) {
+    let userInfo = req.body.userInfo;
+    let serviceInfo = req.body.serviceInfo;
+    if (service.validateUser(userInfo)) {
+      if (service.isExistRoom(serviceInfo)) {
+        if (service.isHost(userInfo, serviceInfo)) {
+          service.createRoom(userInfo, serviceInfo);
+          res.send(service.SuccessCode);
+          res.end();
+          return;
+        }
+      }
+    }
+    res.send(service.FailCode);
+    res.end();
+  },
 };
 
 module.exports = HostController;
